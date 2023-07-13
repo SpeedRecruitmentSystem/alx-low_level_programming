d3589b6c0c25e6b84282a8923d1f4a49be660ddc
Patricia Keele <pjkeele1@gmail.com>
2023-07-13 13:11:26 -0700

    4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*alot is happening here
+*main - and it needs to be followed
+*return : retuns 0 (positive number)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
